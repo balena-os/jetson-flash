a48b59663a5f456f49f92f83a0b8df47598581fe
Marin Muhieddine <marinmuhieddine17@gmail.com>
2026-08-04 16:33:52 +0200

    Adapt build and run script for p12n imagine

--- Orin_Flash/build_and_run.sh
+++ Orin_Flash/build_and_run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Build Dockerfile
 docker build -t orin-image .
 
 # Run resulting Docker image. The balenaOS image downloaded from balena-cloud is expected to exist in the HOST, inside ~/images. That directory will be bind-mounted inside the running container in /data/images/
-docker container run --rm -it --privileged -v /dev/:/dev/ -v ~/images:/data/images orin-image /bin/bash
+docker container run --rm -it --privileged -v /dev/:/dev/ --mount type=bind,source={images_path},target=/data/images orin-image /bin/bash
