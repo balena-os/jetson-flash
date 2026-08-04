ef6aea20567151a2487ee4708fa70ce10c57fb7f
Marin Muhieddine <marinmuhieddine17@gmail.com>
2026-08-04 17:05:50 +0200

    Dynamic image path

--- Orin_Flash/build_and_run.sh
+++ Orin_Flash/build_and_run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Build Dockerfile
+HOST_IMAGES_PATH="${HOST_IMAGES_PATH:"~/images"}"
 docker build -t orin-image .
 
 # Run resulting Docker image. The balenaOS image downloaded from balena-cloud is expected to exist in the HOST, inside ~/images. That directory will be bind-mounted inside the running container in /data/images/
-docker container run --rm -it --privileged -v /dev/:/dev/ --mount type=bind,source={images_path},target=/data/images orin-image /bin/bash
+docker container run --rm -it --privileged -v /dev/:/dev/ --mount type=bind,source=${HOST_IMAGES_PATH},target=/data/images orin-image /bin/bash
